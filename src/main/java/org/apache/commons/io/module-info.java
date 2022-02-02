543ecbb77cc00ee96c7d31d8f9cef95c73cf3a4c
Sruthi Venkat <sruthivenkat@Sruthis-MBP.lan>
2022-02-02 10:05:24 -0500

    add Java 9 modules

---
+++ src/main/java/org/apache/commons/io/module-info.java
@@ -0,0 +1,17 @@
+module org.apache.commons.io {
+	exports org.apache.commons.io;
+	exports org.apache.commons.io.charset;
+	exports org.apache.commons.io.comparator;
+	exports org.apache.commons.io.file;
+	exports org.apache.commons.io.file.attribute;
+	exports org.apache.commons.io.file.spi;
+	exports org.apache.commons.io.filefilter;
+	exports org.apache.commons.io.function;
+	exports org.apache.commons.io.input;
+	exports org.apache.commons.io.input.buffer;
+	exports org.apache.commons.io.monitor;
+	exports org.apache.commons.io.output;
+	exports org.apache.commons.io.serialization;
+	requires java.compiler;
+	requires java.xml;
+}
---
+++ src/test/java/org/apache/commons/io/module-info.java
@@ -0,0 +1,15 @@
+open module org.apache.commons.io {
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.params;
+	requires org.junitpioneer;
+	requires org.mockito;
+	requires org.apache.commons.lang3;
+	requires jmh.core;
+	requires jdk.hotspot.agent;
+	requires jdk.rmic;
+	requires jdk.jartool;
+	requires jdk.crypto.cryptoki;
+	requires jdk.net;
+	requires java.xml;
+	requires java.scripting;
+}
